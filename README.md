4714c55975df14029b5209c1a9160b6968242d6c
Emily Platzer <emilyplatzer@gmail.com>
2016-04-18 16:27:03 -0600

    Added README, reset index.js, added comments to direct some play

---
+++ README.md
@@ -0,0 +1,19 @@
+# README
+
+## This is a playground for messing around with bcrypt in isolation
+
+### Run it locally
+
+* Fork, clone, npm install
+* `$ node index.js`
+
+### How did you set this up?
+
+* Make a directory for this project
+* `$ git init`
+* `$ npm init`
+* `$ touch .gitignore && echo npm_modules > .gitignore`
+* `$ npm install bcrypt --save`
+* `$ touch index.js`
+* Add examples of usage from [npm's bcrypt](https://www.npmjs.com/package/bcrypt)
+* `console.log()` like everything, experiment until things make sense
--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 var bcrypt = require('bcrypt');
 
 var pw = 'B4c0/\/';
-// var storedHash;
-var storedHash = "$2a$10$4/5H4r1se8NCrPDX5Ey9S.J73ScAAF/t.G4hpyZJe7F9ba7UY04Zu";
+var storedHash;
+
+// All these below hashes are crypted forms of the password 'B4c0/\/'
+// Try decrypting any of these hashes by commenting out line 4, 16-25 and
+// un-commenting one of the lines 10, 11, 12, 13, or 14 (you could remove timeout too).
+
+// var storedHash = "$2a$10$4/5H4r1se8NCrPDX5Ey9S.J73ScAAF/t.G4hpyZJe7F9ba7UY04Zu";
 // var storedHash = "$2a$09$hK9vFqya.CId9QJ7Atu.bug2E/JQyoB2fHXIJEo3S5R0isymsesRm";
 // var storedHash = "$2a$10$ts3jWQ2oVZe0fFoY2ZF6qeElQMcElJWGqgiWmqZzY.OqfifrKQS8e";
 // var storedHash = "$2a$09$I28JRb7AvjQpGC5n6FM4QeVIoPDjxN9UVXWKuJFIln952ZPJ8ZvWC";
 // var storedHash = "$2a$10$rG4.97KlYTNK.kLhLBOx8utYsPUwsnd/FZ7HQDCi8/qbgWM0z0vHS";
 
-// bcrypt.genSalt(10, function(err, salt) {
-//   bcrypt.hash(pw, salt, function(err, hash) {
-//     storedHash = hash;
-//     console.log("Password:", pw);
-//     console.log("passed in number = 10");
-//     console.log("salt length      = " + salt.length);
-//     console.log("Salt:    ", salt);
-//     console.log("Hash:    ", hash);
-//   });
-// });
+bcrypt.genSalt(10, function(err, salt) {
+  bcrypt.hash(pw, salt, function(err, hash) {
+    storedHash = hash;
+    console.log("Password:", pw);
+    console.log("passed in number = 10");
+    console.log("salt length      = " + salt.length);
+    console.log("Salt:    ", salt);
+    console.log("Hash:    ", hash);
+  });
+});
 
 setTimeout(function(){
   bcrypt.compare('B4c0/\/', storedHash, function(err, res) {
